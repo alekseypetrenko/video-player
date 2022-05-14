7ad1fa1062d2bc2884bca576130b2cbc183f3172
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2022-05-14 14:08:53 +0300

    fullscreen open/close

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const player = document.querySelector(".player");
 const video = document.querySelector("video");
 const progressRange = document.querySelector(".progress-range");
 const progressBar = document.querySelector(".progress-bar");
@@ -107,6 +108,42 @@ function changeSpeed() {
     video.playbackRate = speed.value;
 }
 // Fullscreen ------------------------------- //
+let fullscreen = false;
+
+function toggleFullscreen() {
+    !fullscreen ? openFullscreen(player) : closeFullscreen();
+    fullscreen = !fullscreen;
+}
+
+/* View in fullscreen */
+function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) {
+        /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+        /* IE11 */
+        elem.msRequestFullscreen();
+    }
+
+    video.classList.add("video-fullscreen");
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+        /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        /* IE11 */
+        document.msExitFullscreen();
+    }
+
+    video.classList.remove("video-fullscreen");
+}
 
 // Event Listeners
 playBtn.addEventListener("click", togglePlay);
@@ -117,3 +154,4 @@ progressRange.addEventListener("click", setProgress);
 volumeRange.addEventListener("click", changeVolume);
 volumeBtn.addEventListener("click", toggleMute);
 speed.addEventListener("change", changeSpeed);
+fullscreenBtn.addEventListener("click", toggleFullscreen);
